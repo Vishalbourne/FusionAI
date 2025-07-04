c4538ed7b427caa7c1c0add8fa5e917abfda8c28
user.name like : "Vishalbourne <user.email like :  vishalbourne007@gmail.com>
2025-07-04 13:06:11 +0530

    Change OAuth redirect URI

--- backend/config/passport-auth.js
+++ backend/config/passport-auth.js
@@ -11,7 +11,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: `${process.env.callbackURL}/auth/google/callback`
 }, async (accessToken, refreshToken, profile, done) => {
   try {
 
@@ -37,7 +37,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback"
+  callbackURL: `${process.env.callbackURL}/auth/github/callback`
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     console.log(profile);
